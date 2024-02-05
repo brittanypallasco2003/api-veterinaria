edaf170a70bb656f710d40762fb977a6536be9aa
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-02-05 00:51:15 -0500

    configuraciones para el deploy

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -5,18 +5,18 @@ dotenv.config()
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
-    host: process.env.HOST_MAIL,
-    port: process.env.PORT_MAIL,
+    host: process.env.HOST_MAILTRAP,
+    port: process.env.PORT_MAILTRAP,
     auth: {
-        user: process.env.USER_MAIL,
-        pass: process.env.PASS_MAIL,
+        user: process.env.USER_MAILTRAP,
+        pass: process.env.PASS_MAILTRAP,
     }
 });
 
 const sendMailToUser = (userMail, token) => {
 
     let mailOptions = {
-        from: process.env.USER_MAIL,
+        from: process.env.USER_MAILTRAP,
         to: userMail,
         subject: "Verifica tu cuenta",
         html: `<p>Hola, haz clic <a href="${process.env.URL_BACKEND}confirmar/${encodeURIComponent(token)}">aquí</a> para confirmar tu cuenta.</p>`
